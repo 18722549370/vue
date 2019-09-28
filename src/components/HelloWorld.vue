325db1d25f925e654684fb202bb78faa7bb3d647
panpan <2294772762@qq.com>
2019-09-28 22:22:35 +0800

    vuex学习

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
+      <li v-for="(item,index) in dataTop" :key="index">{{ item.name }}{{ item.price }}</li>
     </ul>
-    <h2>Ecosystem</h2>
     <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
+      <li v-for="(item,index) in discount" :key="index">{{ item.name }}{{ item.price }}</li>
     </ul>
+    <button @click="storeClickOne">价格-方法一</button>
+    <button @click="storeClickTwo">价格-方法二</button>
   </div>
 </template>
 
@@ -89,6 +18,22 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    dataTop () {
+      return this.$store.state.dataTop
+    },
+    discount () {
+      return this.$store.getters.salePrice //处理数据
+    }
+  },
+  methods: {
+    storeClickOne () {
+      this.$store.commit('btnPrice') //激活事件里的方法 
+    },
+    storeClickTwo () {
+      this.$store.dispatch('timerPrice') //激活异步事件方法
     }
   }
 }
---
+++ src/store/store.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import vuex from 'vuex'
+Vue.use(vuex)
+export default new vuex.Store({
+  state: {  //存储组件需要的数据
+    dataTop: [
+      { name: '马云', price: 120 },
+      { name: '马化腾', price: 180 },
+      { name: '马地', price: 60 },
+      { name: '马天', price: 10 }
+    ] 
+  },
+  getters: {  //处理store.js中的数据
+    salePrice(state){
+      let discount = state.dataTop.map((disNum) => {
+        return {
+          name: '--' + disNum.name + '--',
+          price: disNum.price/2
+        }
+      })
+      return discount 
+    }
+  },
+  mutations: { //处理store.js中的事件方法
+    btnPrice(state){
+      state.dataTop.forEach((item) => {
+        item.price-=1
+      })
+    }
+  },
+  actions: { //异步处理mutations里面的事件方法
+    timerPrice(context){
+      setTimeout(function(){
+        context.commit('btnPrice')
+      }, 1000)
+    }
+  }
+})
